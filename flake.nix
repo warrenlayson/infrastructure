07354ef7b3de8c864e1a4761482057c038cd4f58
Warren Layson <warrenlayson@gmail.com>
2025-05-09 17:43:22 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+    description = "Infrastructure";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem(system: 
+            let 
+                pkgs = (import (nixpkgs) {
+                    inherit system;
+                    config = {
+                        allowUnfree = true;
+                    };
+                });
+            in {
+                devShell = pkgs.mkShell {
+                    buildInputs = [
+                        pkgs.terraform
+                    ];
+
+                    };
+                }
+        );
+}
