ac10064a7425ba438a045053988ab224582b4b8c
Warren Layson <warrenlayson@gmail.com>
2025-05-09 17:58:52 +0800

    chore: add pre-commit

--- flake.nix
+++ flake.nix
@@ -1,25 +1,37 @@
 {
-    description = "Infrastructure";
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem(system: 
-            let 
-                pkgs = (import (nixpkgs) {
-                    inherit system;
-                    config = {
-                        allowUnfree = true;
-                    };
-                });
-            in {
-                devShell = pkgs.mkShell {
-                    buildInputs = [
-                        pkgs.terraform
-                    ];
+  description = "Infrastructure";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
+  outputs = { self, nixpkgs, flake-utils, pre-commit-hooks, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import (nixpkgs) {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      in
+      {
+        checks = {
+          pre-commit = pre-commit-check;
+        };
+        devShell = pkgs.mkShell {
+          inherit (pre-commit-check) shellHook;
+          buildInputs = [
+            pkgs.terraform
+          ] ++ pre-commit-check.enabledPackages;
 
-                    };
-                }
-        );
+        };
+      }
+    );
 }
